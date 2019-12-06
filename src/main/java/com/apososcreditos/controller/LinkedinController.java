2319d43bcb1d6f9d27e8d8c11f530caa866e720a
Carlos Moisés B. Henrique <moizezhenrique@gmail.com>
2019-12-06 15:28:50 -0300

    Login com o Linked In

--- src/main/java/com/apososcreditos/controller/LinkedinController.java
+++ src/main/java/com/apososcreditos/controller/LinkedinController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.social.facebook.api.User;
+import org.springframework.social.linkedin.api.LinkedInProfileFull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,16 +17,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.apososcreditos.model.UserInfo;
-import com.apososcreditos.service.FacebookService;
+import com.apososcreditos.service.LinkedinService;
 import com.apososcreditos.service.SecurityService;
 import com.apososcreditos.service.SessionService;
 import com.apososcreditos.service.UserService;
 
 @Controller
-public class FacebookController {
+public class LinkedinController {
 
 	@Autowired
-	private FacebookService facebookService;
+	private LinkedinService linkedinService;
 	@Autowired
 	private UserService userService;
 	@Autowired
@@ -34,38 +34,38 @@ public class FacebookController {
 	@Autowired
 	private SecurityService securityService;
 
-	@GetMapping(value = "/facebooklogin")
-	public RedirectView facebooklogin() {
+	@GetMapping(value = "/linkedinlogin")
+	public RedirectView linkedinlogin() {
 		RedirectView redirectView = new RedirectView();
-		String url = facebookService.facebooklogin();
+		String url = linkedinService.linkedinlogin();
 		System.out.println(url);
 		redirectView.setUrl(url);
 
 		return redirectView;
 	}
 
-	@GetMapping(value = "/facebook")
-	public String facebook(@RequestParam("code") String code) {
-		String accessToken = facebookService.getFacebookAccessToken(code);
-		return "redirect:/facebookprofiledata/" + accessToken;
+	@GetMapping(value = "/linkedin")
+	public String linkedin(@RequestParam("code") String code) {
+		String accessToken = linkedinService.getLinkedInAccessToken(code);
+		return "redirect:/linkedinprofiledata/" + accessToken;
 
 	}
 
-	@GetMapping(value = "/facebookprofiledata/{accessToken:.+}")
-	public String facebookprofiledata(@PathVariable String accessToken, Model model, HttpServletRequest request) {
-		User user = facebookService.getFacebookUserProfile(accessToken);
-		UserInfo dbUser = userService.findByEmail(user.getEmail());
+	@GetMapping(value = "/linkedinprofiledata/{accessToken:.+}")
+	public String linkedinprofiledata(@PathVariable String accessToken, Model model, HttpServletRequest request) {
+		LinkedInProfileFull user = linkedinService.getLinkedInUserProfile(accessToken);
+		UserInfo dbUser = userService.findByEmail(user.getEmailAddress());
 		String role = "USER";
 		if (dbUser != null) {
 			dbUser.setFirstName(user.getFirstName());
 			dbUser.setLastName(user.getLastName());
-			//dbUser.setImage(user.getCover().getSource());
+			dbUser.setImage(user.getProfilePictureUrl());
 			userService.update(dbUser);
 			role = dbUser.getRole();
 			model.addAttribute("user", dbUser);
 		} else {
-			UserInfo userInfo = new UserInfo(user.getFirstName(), user.getLastName(), "");
-			userInfo.setEmail(user.getEmail());
+			UserInfo userInfo = new UserInfo(user.getFirstName(), user.getLastName(), user.getProfilePictureUrl());
+			userInfo.setEmail(user.getEmailAddress());
 			userInfo.setEnabled(true);
 			userInfo.setRole("USER");
 			userService.save(userInfo);
@@ -73,9 +73,8 @@ public class FacebookController {
 			model.addAttribute("user", userInfo);
 
 		}
-		System.out.println("PASSOU AQUI");
 		sessionService.criarSession("usuario-logado", dbUser);
-		securityService.autoLogin(user.getEmail(), null, role, request);
+		securityService.autoLogin(user.getEmailAddress(), null, role, request);
 
 		String name = SecurityContextHolder.getContext().getAuthentication().getName();
 		Collection<? extends GrantedAuthority> grantedAuthorities = SecurityContextHolder.getContext()
@@ -84,7 +83,7 @@ public class FacebookController {
 		while (iterator.hasNext()) {
 			System.out.println(iterator.next());
 		}
-		
+
 		System.out.println(name);
 		return "redirect:/redirectdashboard";
 	}
--- src/main/java/com/apososcreditos/service/LinkedInServiceImpl.java
+++
@@ -1,5 +0,0 @@
-package com.apososcreditos.service;
-
-public class LinkedInServiceImpl {
-
-}
